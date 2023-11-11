880a5c98b0f003a638ed55d4ffbb4234c75822cd
Frans Johansson <fransjohansson98@gmail.com>
2023-11-11 19:36:23 +0100

    Revert to neo-tree as filetree plugin

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,21 +1,32 @@
 -- Nicer filetree than NetRW
 return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup()
+			require("helpers.keys").map(
+				{ "n", "v" },
+				"<leader>e",
+				"<cmd>NeoTreeRevealToggle<cr>",
+				"Toggle file explorer"
+			)
+		end,
+	},
 	-- {
-	-- 	"nvim-neo-tree/neo-tree.nvim",
-	-- 	branch = "v2.x",
+	-- 	"nvim-tree/nvim-tree.lua",
+	-- 	version = "*",
+	-- 	lazy = false,
 	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
 	-- 		"nvim-tree/nvim-web-devicons",
-	-- 		"MunifTanjim/nui.nvim",
 	-- 	},
 	-- 	config = function()
-	-- 		require("neo-tree").setup()
-	-- 		require("helpers.keys").map(
-	-- 			{ "n", "v" },
-	-- 			"<leader>e",
-	-- 			"<cmd>NeoTreeRevealToggle<cr>",
-	-- 			"Toggle file explorer"
-	-- 		)
+	-- 		require("nvim-tree").setup({})
 	-- 	end,
 	-- },
 	-- {
@@ -25,18 +36,18 @@ return {
 	-- 		require("oil").setup({})
 	-- 	end,
 	-- },
-	{
-		"echasnovski/mini.files",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		version = "*",
-		config = function()
-			local minifiles = require("mini.files")
-			minifiles.setup({})
-			require("helpers.keys").map({ "n", "v" }, "<leader>e", function()
-				if minifiles.close() == nil then
-					minifiles.open(vim.api.nvim_buf_get_name(0))
-				end
-			end, "Toggle file explorer")
-		end,
-	},
+	-- {
+	-- 	"echasnovski/mini.files",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- 	version = "*",
+	-- 	config = function()
+	-- 		local minifiles = require("mini.files")
+	-- 		minifiles.setup({})
+	-- 		require("helpers.keys").map({ "n", "v" }, "<leader>e", function()
+	-- 			if minifiles.close() == nil then
+	-- 				minifiles.open(vim.api.nvim_buf_get_name(0))
+	-- 			end
+	-- 		end, "Toggle file explorer")
+	-- 	end,
+	-- },
 }
